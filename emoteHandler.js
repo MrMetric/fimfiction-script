27fb05e06814f17bb32bceeb938382f303bf7e43
KrazyTheFox <traveler@krazyweb.net>
2012-12-20 02:37:26 -0500

    Clicking emotes adds them to the comment box.

--- emoteHandler.js
+++ emoteHandler.js
@@ -269,7 +269,7 @@ function initializeAPI()
 	br = document.createElement("br");
 	emotePanel.insertBefore(br, emotePanel.firstChild);
 
-	for(var i = 0; i < emoteTables.length / 7 - 1; i++)
+	for(var i = 0; i < emoteTables.length / 7 + 1; i++)
 	{
 		br = document.createElement('br');
 		emotePanel.insertBefore(br, emotePanel.firstChild);
@@ -358,7 +358,7 @@ function createNewEmote(url, tableName)
 	image.width = "54";
 	image.height = "54";
 	image.style.margin = "5px";
-	//image.addEventListener("click", function() { addAnEmote(this.id); }, false);
+	image.addEventListener("click", function() { addEmoteToCommentBox(this.id); }, false);
 	emoteTables[tablePrefix + tableName].appendChild(image);
 }
 
@@ -392,4 +392,58 @@ function showTable(tableID)
 			emoteTables[table].style.display = 'none';
 		}
 	}
+}
+
+function addEmoteToCommentBox(url)
+{
+	
+	replaceSelectedText(commentBox, "[img]" + url + "[/img]");
+	
+}
+
+function getInputSelection(el) {
+    var start = 0, end = 0, normalizedValue, range,
+        textInputRange, len, endRange;
+
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    } else {
+        range = document.selection.createRange();
+
+        if (range && range.parentElement() == el) {
+            len = el.value.length;
+            normalizedValue = el.value.replace(/\r\n/g, "\n");
+			
+            textInputRange = el.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
+			
+            endRange = el.createTextRange();
+            endRange.collapse(false);
+
+            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                start = end = len;
+            } else {
+                start = -textInputRange.moveStart("character", -len);
+                start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                    end = len;
+                } else {
+                    end = -textInputRange.moveEnd("character", -len);
+                    end += normalizedValue.slice(0, end).split("\n").length - 1;
+                }
+            }
+        }
+    }
+
+    return {
+        start: start,
+        end: end
+    };
+}
+
+function replaceSelectedText(el, text) {
+    var sel = getInputSelection(el), val = el.value;
+    el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
 }
