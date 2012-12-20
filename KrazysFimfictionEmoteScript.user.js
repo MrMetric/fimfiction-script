af0ed98edcbb334d03d7727d7c45e0d208c50985
KrazyTheFox <traveler@krazyweb.net>
2012-12-20 13:44:46 -0500

    Added new location of remote script and updated name.

--- KrazysFimfictionEmoteScript.user.js
+++ KrazysFimfictionEmoteScript.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name        Fimfiction Emotes TEST
-// @namespace   ffemotetest
-// @description Adds additional emotes to fimfiction, taken from reddit.
+// @name        Fimfiction Emote Extender
+// @namespace   ffemoteextender
+// @description Adds additional emotes to fimfiction.net.
 // @include     http*://www.fimfiction.net/*
 // @include     http*://fimfiction.net/*
 // @grant       none
 // @require		https://github.com/iloveportalz0r/fimfiction-script/raw/master/emoteHandler.js
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
-// @version     0.1
+// @version     1.0
 // ==/UserScript==
 
 window.addEventListener("load", function load(event){
@@ -19,7 +19,7 @@ function run() {
 	
 	initializeAPI();
 	
-	$.getScript("http://www.krazyweb.net/fimfictionemotes/RemoteEmotes.js", function(){
+	$.getScript("https://github.com/iloveportalz0r/fimfiction-script/raw/master/RemoteEmotes.js", function(){
 	   
 		var emotes = fetchAndAddEmotes();
 	   
